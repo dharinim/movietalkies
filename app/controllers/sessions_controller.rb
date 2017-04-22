fb749d8ce8bfed9f6ed0fe9ee302cb68bb5428c2
Dharini Murugaprabhu <dharinishree@gmail.com>
2017-04-22 13:08:08 -0700

    add session and user controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,25 @@
 class SessionsController < ApplicationController
+  include SessionsHelper
+  def new
+  end
+
+  def create
+    @user = User.find_by_email(params[:session][:email])
+    if @user && @user.authenticate(params[:session][:password])
+      # session[:user_id] = @user.id
+      # redirect_to '/'
+      login(@user)
+      redirect_to @user
+    else
+      # redirect_to 'login'
+      # flash[:danger] = "Invalid email and/or password."
+      redirect_to login_url, notice: 'Invalid email and/or password.'
+    end
+  end
+
+  def destroy
+    # session[:user_id] = nil
+    log_out
+    redirect_to '/'
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,58 @@
 class UsersController < ApplicationController
+  include SessionsHelper
+
+  before_action :require_user, only: [:index, :show]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+  end
+  
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+  
+  def edit
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login(@user)
+      # session[:user_id] = @user.id
+      # redirect_to '/'
+      # flash[:success] = "Welcome"
+      redirect_to @user, notice: 'User was successfully created.'
+    else
+      # redirect_to '/signup'
+      render 'new'
+    end
+  end
+  
+  def update
+    if @user.update(user_params)
+        redirect_to @user, notice: 'User was successfully updated.' 
+    else
+        render :edit 
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_url, notice: 'User was successfully destroyed.' 
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
+
 end
+
