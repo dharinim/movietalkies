879aa0ac762962ddf802f854dfd140639145ce9c
Dharini Murugaprabhu <dharinishree@gmail.com>
2017-04-22 23:26:14 -0700

    add ajax calls for signup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,48 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$.ajaxSettings.dataType = "json";
+$(document).ready(function(){
+    $("#new_user").submit(function(e){
+    e.preventDefault();
+    
+    $("#new_user").on("ajax:success", function (e, data, status, xhr){
+      console.log("success")
+      $("#new_user")[0].reset();
+    });
+
+    $("#new_user").on("ajax:error", function (e, data, status, xhr){
+      console.log("error")
+      $("#new_user")[0].reset();
+    });
+
+
+    $("#new_user").on("ajax:complete", function (e, data, status, xhr){
+      console.log("complete")
+      $("#new_user")[0].reset();
+    });
+
+ 
+  });
+  // .done(function(){console.log("dfsfsdf")
+
+  //   $(this)[0].reset();
+  // });
+    // $("#new_user").on("ajax:success", function (e, data, status, xhr){
+  //     console.log("jhbjkhk")
+  //   $("#new_user")[0].reset();
+  // });
+});
+
+// $.ajaxSettings.dataType = "json";
+// $(document).ready(function (){
+//   $("#new_attendance").on("ajax:success", function (e, data, status, xhr){
+//     $('#ateendee-list').append($("<li>" + data.first_name + "</li>"));
+//     $("#attendance-add").hide()
+//   });
+//   $("#new_attendance").on("ajax:error", function (e, xhr, status, error){
+//     alert(error);
+//   });
+// })
+
